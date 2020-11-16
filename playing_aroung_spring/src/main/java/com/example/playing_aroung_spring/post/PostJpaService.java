6cb3ba4d572916b689b79caa28a1a0bc437c5c6a
Ladislav <ladislav.szabo01@t-systems.com>
2020-11-16 13:10:49 +0100

    Removing not related module

--- playing_aroung_spring/src/main/java/com/example/playing_aroung_spring/post/PostJpaService.java
+++
@@ -1,6 +0,0 @@
-package com.example.playing_aroung_spring.post;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PostJpaService extends JpaRepository<Post,Integer> {
-}
--- playing_aroung_spring/src/main/resources/data.sql
+++
@@ -1,6 +0,0 @@
-insert into user values(1000, sysdate(), 'Laci');
-insert into user values(1001, sysdate(), 'Dodo');
-insert into user values(1002, sysdate(), 'Katka');
-insert into post values(1100,'a',1000);
-insert into post values(1101,'b',1000);
-insert into post values(1102,'c',1001);
